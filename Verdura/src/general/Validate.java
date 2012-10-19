e95a81b6a945f6b856c21af5b905eca0958d0fd7
leyner.castillo <leyner.castillo@02-COM-BQT-TIC.comlago.local>
2012-10-19 16:43:09 -0430

    Optimizando funciones ya existentes, aplicando javadocs y bautizandola como la primera version.

--- Verdura/src/general/Validate.java
+++ Verdura/src/general/Validate.java
@@ -7,17 +7,19 @@ import java.util.regex.Pattern;
 
 /**
  * @author Leyner
- *
+ * @since 18/10/2012
+ * @version 0.1 Update 19/10/12
  */
 public class Validate {
 
 	/**
 	 * @param string
-	 * @return
+	 * @return <b>True</b> Si <i>string</i> no esta vacio. <br>
+	 *         <b>False</b> Si <i>string</i> esta vacio.
 	 */
 	public boolean noEmpty(String string){
 		String string2 = string.replace(" +", " ").trim();
-		if(string2.isEmpty())
+		if(!string2.isEmpty())
 			return true;
 		else
 			return false;
@@ -25,7 +27,8 @@ public class Validate {
 
 	/**
 	 * @param email
-	 * @return
+	 * @return <b>True</b> Si <i>email</i> es un email. <br>
+	 *         <b>False</b> Si <i>email</i> no es un email.
 	 */
 	public boolean isEmail(String email){
 		Pattern pattern = null;
@@ -40,7 +43,8 @@ public class Validate {
 
 	/**
 	 * @param number
-	 * @return
+	 * @return <b>True</b> Si <i>number</i> no es cero. <br>
+	 *         <b>False</b> Si <i>number</i> es cero.
 	 */
 	public boolean noZero(Integer number){
 		if (!number.equals(0) || !number.equals(null))
@@ -51,23 +55,13 @@ public class Validate {
 
 	/**
 	 * @param string
-	 * @return
+	 * @param compare
+	 * @return <b>True</b> Si <i>string</i> es igual a <i>compare</i> <br>
+	 *         <b>False</b> Si <i>string</i> no es igual <i>compare</i>.
 	 */
-	public boolean noSeleccione(String string){
+	public boolean equalString(String string, String compare){
 		String string2 = string.replace(" +", " ").trim();
-		if (string2.equals("--Seleccione--"))
-			return true;
-		else
-			return false;
-	}
-	
-	/**
-	 * @param string
-	 * @return
-	 */
-	public boolean noGuion(String string){
-		String string2 = string.replace(" +", " ").trim();
-		if (string2.equals("--"))
+		if (string2.equals(compare))
 			return true;
 		else
 			return false;
@@ -75,10 +69,11 @@ public class Validate {
 
 	/**
 	 * @param date
-	 * @return
+	 * @return <b>True</b> Si <i>date</i> tiene fecha menor al dia de hoy.<br>
+	 *         <b>False</b> Si <i>date</i> no tiene fecha menor al dia de hoy.
 	 */
-	public boolean noPast(Calendar date){
-		if (!date.before(new Date()))
+	public boolean datePast(Calendar date){
+		if (date.before(new Date()))
 			return true;
 		else
 			return false;
@@ -86,10 +81,11 @@ public class Validate {
 
 	/**
 	 * @param date
-	 * @return
+	 * @return <b>True</b> Si <i>date</i> tiene fecha mayor al dia de hoy.<br>
+	 *         <b>False</b> Si <i>date</i> no tiene fecha mayor al dia de hoy.
 	 */
-	public boolean noFuture(Calendar date){
-		if (!date.after(new Date()))
+	public boolean dateFuture(Calendar date){
+		if (date.after(new Date()))
 			return true;
 		else
 			return false;
